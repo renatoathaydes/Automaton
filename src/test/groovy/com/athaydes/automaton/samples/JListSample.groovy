a7ae50cfac2635fd4bc3d9af1b807692151570ce
renatoathaydes <renatoathaydes@hotmail.com>
2015-10-11 10:25:27 +0200

    Improved JListSample style

--- src/test/groovy/com/athaydes/automaton/samples/JListSample.groovy
+++ src/test/groovy/com/athaydes/automaton/samples/JListSample.groovy
@@ -14,9 +14,9 @@ class JListSample {
     JFrame frame
 
     @Before
-    void before( ) {
+    void before() {
         frame = new JFrame( 'JList Test' )
-        frame.layout = new FlowLayout( )
+        frame.layout = new FlowLayout()
 
         String[] listItems = [ 'green', 'red', 'orange', 'dark blue', 'yellow', 'black', 'mauve', 'brown', 'purple' ]
         JList list = new JList( listItems )
@@ -24,27 +24,27 @@ class JListSample {
         list.visibleRowCount = 3
 
         frame.add( new JScrollPane( list ) )
-        frame.pack( )
+        frame.pack()
 
         frame.visible = true
     }
 
     @After
-    void after( ) {
+    void after() {
         frame?.dispose()
     }
 
     @Test
-    void "List items on a multi-column JList can be clicked on using a text selector" () {
+    void "List items on a multi-column JList can be clicked on using a text selector"() {
         // Use the swinger to click on JList cells in each column
-        Swinger swing = Swinger.forSwingWindow( )
+        Swinger swing = Swinger.forSwingWindow()
 
         swing.clickOn( 'text:black', Speed.VERY_FAST ).pause( 250 )
-        swing.clickOn( 'text:green', Speed.VERY_FAST ).pause( 250 )
-        swing.clickOn( 'text:dark blue', Speed.VERY_FAST ).pause( 250 )
-        swing.clickOn( 'text:brown', Speed.VERY_FAST ).pause( 250 )
-        swing.clickOn( 'text:red', Speed.VERY_FAST ).pause( 250 )
-        swing.clickOn( 'text:purple', Speed.VERY_FAST )
+                .clickOn( 'text:green', Speed.VERY_FAST ).pause( 250 )
+                .clickOn( 'text:dark blue', Speed.VERY_FAST ).pause( 250 )
+                .clickOn( 'text:brown', Speed.VERY_FAST ).pause( 250 )
+                .clickOn( 'text:red', Speed.VERY_FAST ).pause( 250 )
+                .clickOn( 'text:purple', Speed.VERY_FAST )
     }
 
 }
